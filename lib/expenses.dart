043ec5b6acd26521710aa378430f3ecb741cb85a
Harsh Kumar <harsh20221@gmail.com>
2024-03-22 15:57:56 +0530

    formatting

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,23 +1,34 @@
 import 'package:expansetracker/models/expense.dart';
-import  'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
-const Expenses({super.key});
+  const Expenses({super.key});
   @override
-State <Expenses> createState(){ ///! Write createState 's c in small and S in capital 
-  return _ExpensesState();
-
-} 
-  
+  State<Expenses> createState() {
+    ///! Write createState 's c in small and S in capital
+    return _ExpensesState();
+  }
 }
 
-class _ExpensesState extends State <Expenses> {
-  
-List <expense> _registeredexpenses = [
-  expense(title: 'Flutter Course ', amount: 19.99, date: DateTime.now(), category: Category.work),expense(title:'Cinema' , amount: 15.99, date: DateTime.now(), category: Category.leisure)
-];
+class _ExpensesState extends State<Expenses> {
+  List<expense> _registeredexpenses = [
+    expense(
+        title: 'Flutter Course ',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    expense(
+        title: 'Cinema',
+        amount: 15.99,
+        date: DateTime.now(),
+        category: Category.leisure)
+  ];
   @override
-  Widget build(BuildContext context){  ///! write build's b in small letter and inside write BuildContext's B in capital letters 
-    return Scaffold(body:Column(children: [Text("I LOVE YOU ")],));
+  Widget build(BuildContext context) {
+    ///! write build's b in small letter and inside write BuildContext's B in capital letters
+    return Scaffold(
+        body: Column(
+      children: [Text("I LOVE YOU ")],
+    ));
   }
 }
