a06e8ed63ddf69e3f5c376d4607612aad53f5f59
Harsh Kumar <harsh20221@gmail.com>
2024-03-22 21:27:16 +0530

    completed with title ans date

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -9,7 +9,7 @@ class expense_item extends StatelessWidget{
     return  Card(   
         child:Padding(padding: EdgeInsets.symmetric(horizontal: 20,vertical: 16),child: Column(
           children: [
-            Text(expense.title), const SizedBox(height: 4,)
+            Text(expense.title), const SizedBox(height: 4),Row(children: [Text('\$${expense.amount.toStringAsFixed(2)}'),Spacer(),Row(children: [Icon(Icons.alarm),const SizedBox(width: 8,),Text(expense.date.toString())],)],)
           ],
         ),) ///! Make sure to enclose the expense.title inside Text widget
     ); ///! Make sure Padding is Inside the Card Widget 
