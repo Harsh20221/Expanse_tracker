5c936ab2da8d1b0ea1d5962930481907b64f7b7f
Harsh Kumar <harsh20221@gmail.com>
2024-03-22 17:24:29 +0530

    changed from text diaplay to card display style

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,3 +1,4 @@
+import 'package:expansetracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart'; ///? In this file we are creating a new widget called expenses_list that will display the list of expenses on the screen
 import 'package:expansetracker/models/expense.dart';
 
@@ -9,7 +10,8 @@ class expenses_list extends StatelessWidget {
     return ListView.builder(  //?  we are using ListView.builder to create a list of expenses that will be displayed on the screen ,
     ///? it's speciality is that it only renders the items that are visible on the screen
         itemCount: expenses.length,  //? itemCount is the number of items that we want to display in the list
-        itemBuilder: ((context, index) => Text(expenses[index].title)));  
+        itemBuilder: ((context, index) => expense_item(expenses[index]))); ///? Here we are using expense_item class 
+        ///?that we created inside expense_item file to display the list in the form of a card style instead of simple text 
   }  //? itemBuilder is a function that takes two arguments context and index and returns a widget that will be displayed at that index
 }
   
