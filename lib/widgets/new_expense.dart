0ffce04f87a6b9bd7c80d57d421f1276974a1113
Harsh Kumar <harsh20221@gmail.com>
2024-03-23 17:26:37 +0530

    Created New expense Window to Add new expenses to our app

---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class New_Expense extends StatefulWidget {
+  const New_Expense({super.key});
+  @override
+  State<NewExpense> createState() {
+    return _NewExpenseState(); //! Return this in the form of a function with ()
+  }
+}
+
+class _NewExpenseState extends State<New_Expense> {
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            maxLength: 50,
+            decoration: InputDecoration(),
+          )
+        ],
+      ),
+    );
+  }
+}
