246f75c92d9814adbd42c4391cb3e617b0fa8272
Harsh Kumar <harsh20221@gmail.com>
2024-03-23 10:35:53 +0530

    Appbar Title

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -32,7 +32,7 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     ///! write build's b in small letter and inside write BuildContext's B in capital letters
-  return Scaffold(appBar: AppBar(actions: [IconButton(onPressed: (){}, icon: Icon(Icons.add))],), //? This is how you'll add  a addbutton along with appbar inside scaffold   , Icon Button is a special button type that we are using inside actions paremeter 
+  return Scaffold(appBar: AppBar(title: Text("Flutter Expense Tracker "),actions: [IconButton(onPressed: (){}, icon: Icon(Icons.add))],), //? This is how you'll add  a addbutton along with appbar inside scaffold   , Icon Button is a special button type that we are using inside actions paremeter 
         body: Column(  //todo: Do not forget to add Appbar function after appbar: argument inside scaffold then define a i\IconButton inside actions parameter of Appbar function 
         //todo:then also remember to use function Icon() then write Icons.add inside the Icon function  , don't just write Icons.add without function definition 
       children: [
