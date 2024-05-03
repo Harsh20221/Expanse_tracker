74670afcf3ecedf24d8ae5ba17b6ef2412326132
Harsh Kumar <harsh20221@gmail.com>
2024-05-04 02:06:11 +0530

    Orientation lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:expansetracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 var kcolorscheme = ColorScheme.fromSeed(
   seedColor: Color.fromARGB(255, 96, 59, 181),
 ); //? Creating a color scheme for the app
 //* This file is the main file from where everything runs
 //* it's the entry point of the app
 var kdarkcolorscheme=ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 5, 99, 125,),brightness: Brightness.dark); //? We added this variable to store theme color  for dark mode 
-void main() {
-  runApp(
+void main() { WidgetsFlutterBinding.ensureInitialized(); //?We have created this Ensure initialised to run app after ui is set to portrait only 
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,]).then((fn){runApp(  //#This System Chrome and set preffered orientation locks device orientation to portait only 
     MaterialApp(darkTheme: ThemeData.dark().copyWith(useMaterial3: true,colorScheme: kdarkcolorscheme), //# There's a special parameter for dark theme in flutter 
         theme: ThemeData().copyWith(
             //# Using this Themedata We are providing basic theme for vairous elements in our app
@@ -32,5 +32,5 @@ void main() {
                     color: kcolorscheme.onSecondaryContainer,
                     fontSize: 14))),themeMode: ThemeMode.system,//? Here we set the theme to be default as system theme either dark or light as selected by user 
         home: const Expenses()),
-  ); //? Adding Themedata then using Material 3= true will change appbar from blue to White
-} //
+  ); //? Adding Themedata then using Material 3= true will change appbar from blue to White});
+ });} //
